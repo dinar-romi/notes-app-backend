d20c20c1adbb4405f71a6b0d112882a9d352f6c9
dinar-romi <dinarromi.pc@gmail.com>
2024-07-14 03:43:29 +0700

    LP - 14-07-2024 postgres database implemented

--- src/server.js
+++ src/server.js
@@ -1,18 +1,21 @@
-const Hapi = require('@hapi/hapi');
-const notes = require('./api/notes');
-const NoteService = require('./services/inMemory/NotesService');
-const NotesValidator = require('./validator/notes');
-const ClientError = require('./exceptions/ClientError');
+// mengimpor dotenv dan menjalankan konfigurasinya
+require("dotenv").config();
+
+const Hapi = require("@hapi/hapi");
+const notes = require("./api/notes");
+const NoteService = require("./services/postgres/NotesService");
+const NotesValidator = require("./validator/notes");
+const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
   const notesService = new NoteService();
 
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
@@ -25,14 +28,14 @@ const init = async () => {
     },
   });
 
-  server.ext('onPreResponse', (request, h) => {
+  server.ext("onPreResponse", (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
 
     // penanganan client error secara internal.
     if (response instanceof ClientError) {
       const newResponse = h.response({
-        status: 'fail',
+        status: "fail",
         message: response.message,
       });
       newResponse.code(response.statusCode);
